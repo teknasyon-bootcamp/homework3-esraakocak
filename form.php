223fd734c2aa0cd11dee38598385defda2d174a2
esraakocak <esra1kocak@gmail.com>
2021-09-03 22:26:12 +0300

    Homework3

--- form.php
+++ form.php
@@ -24,3 +24,57 @@
  * 
  * > **Not: İsteyenler `app2.php` ve `form2.php` isminde dosyalar oluşturup sınıfa farklı özellikler kazandırabilir.
  */
+class Form
+{
+   
+    private string $action;
+    private array $fields;
+    private string $method;
+
+
+
+    function __construct($action, $method)
+    {
+        $this->action = $action;
+        $this->method = $method;
+    }
+
+    public static function createPostForm(string $action)
+    {
+        return new static ($action,"POST");
+    }
+    public static function createGetForm(string $action)
+    {
+        return new static ($action,"GET");
+    }
+
+   public static function createForm(string $action,string $method) {
+       return new static($action,$method);
+            
+   }
+
+   public function addField(string $label, string $name, ?string $defaultValue=null):void {
+          $field=[$label,$name,$defaultValue];
+          $this->fields[] = $field;
+
+   }
+   public function setMethod (string $method):void {
+         $this->method=$method;
+   }
+
+   public function render ():void {
+         
+      echo " <form method='$this->method' action='$this->action' >";
+      foreach ($this->fields as $field) { // 
+        echo "<label for='$field[1]'>$field[0]</label> &nbsp"; 
+        echo "<input type='text' name='$field[1]'  value='$field[2]' />&nbsp"; 
+    }
+    echo "<button type='submit'>Gönder</button>"; 
+    echo "</form>"; 
+}
+
+       
+
+   }
+
+
